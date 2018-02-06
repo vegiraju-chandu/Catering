ba0c17a12cac6ade9ffeea7c04c7d560147969fe
rvegiraju-lavu <appalaraju.in@lavu.com>
2018-02-06 18:47:31 +0530

    Added main course varieties tableview and removed cache from git

--- Catering/Catering/MainViewController.swift
+++ Catering/Catering/MainViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class MainViewController: ParentViewController {
+class MainViewController: ParentViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -26,6 +26,14 @@ class MainViewController: ParentViewController {
             let userProfile = UserProfileController();
             self.present(userProfile, animated: true, completion: nil);
         }
+    }
+    
+    func createMainOptionsContens(){
+        let varietiesTableView = UITableView();
+        varietiesTableView.frame = CGRect(x: 0, y: self.navigationItem.frame.maxY, width: self.bounds.size.width, height: self.view.bounds.size.height - self.navigationItem.frame.maxY );
+        varietiesTableView.delegate = self;
+        varietiesTableView.dataSource = self;
+        self.view.addSubview(varietiesTableView);
     }
     
     
